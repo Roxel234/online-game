d61d737d03796a1508baca4e5eac5f9be5f53c88
Roxel234 <122231025+Roxel234@users.noreply.github.com>
2023-06-11 09:59:57 -0700

    FireBase Configuration
    Needed to get access to database from the server

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+export { fireConfig };
+const fireConfig = {
+  apiKey: "AIzaSyBY5JuHNIEiGhD3Ryjpx2FQd5E-70ks8dI",
+  authDomain: "nodejs-linode-1.firebaseapp.com",
+  databaseURL: "https://nodejs-linode-1-default-rtdb.firebaseio.com",
+  projectId: "nodejs-linode-1",
+  storageBucket: "nodejs-linode-1.appspot.com",
+  messagingSenderId: "383623379741",
+  appId: "1:383623379741:web:06552fe557f1897cf97ff4",
+  measurementId: "G-3YDCR830BN"
+};
