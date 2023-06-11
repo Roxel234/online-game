63d193542a7c9f9b15b0b74f8b4f4a45cf36ba8c
Roxel 234 <rodrguezruben28@gmail.com>
2023-06-11 10:13:12 -0700

    pre-update 2

--- fireConfig.js
+++ fireConfig.js
@@ -1,11 +1,10 @@
 export { fireConfig };
 const fireConfig = {
-  apiKey: "AIzaSyBY5JuHNIEiGhD3Ryjpx2FQd5E-70ks8dI",
-  authDomain: "nodejs-linode-1.firebaseapp.com",
-  databaseURL: "https://nodejs-linode-1-default-rtdb.firebaseio.com",
-  projectId: "nodejs-linode-1",
-  storageBucket: "nodejs-linode-1.appspot.com",
-  messagingSenderId: "383623379741",
-  appId: "1:383623379741:web:06552fe557f1897cf97ff4",
-  measurementId: "G-3YDCR830BN"
+  apiKey: "AIzaSyDS8vN-wwgm9c50ZIGlluVhNrZC0LxcbZk",
+  authDomain: "nodejs-proyect-1.firebaseapp.com",
+  databaseURL: "https://nodejs-proyect-1-default-rtdb.firebaseio.com",
+  projectId: "nodejs-proyect-1",
+  storageBucket: "nodejs-proyect-1.appspot.com",
+  messagingSenderId: "150063946661",
+  appId: "1:150063946661:web:5a98375a82b52d3f30e64c"
 };
